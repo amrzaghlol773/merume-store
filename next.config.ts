a86a1585c2c8f8b30bfd7a9fffadb2e10da20f9e
amrzaghlol773 <amrzaghlol773@gmail.com>
2026-04-05 16:39:36 +0200

    Fix: upload product images to public folder

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      {
+      protocol: 'https',
+      hostname: 'aqmdukkynpvnwaoolyyq.supabase.co',
+    },
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "**" },
       { protocol: "http", hostname: "**" },
